822a60f61338bcb2fb24e3560a4ee98094a28904
NZ <147848898+yzassin@users.noreply.github.com>
2024-01-11 23:04:00 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,22 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+st.set_page_config(
+        page_title="Home",
+        page_icon="📊",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+st.title('Global Missing Migrants')
+st.image('pic.png', use_column_width=True)
 
-    st.sidebar.success("Select a demo above.")
 
-    st.markdown(
+st.write("*__Background, overview and motivation:__*")
+st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+        The global phenomenon of missing migrants is a complex and pressing issue that demands thorough investigation and understanding. Forced or voluntary migration has become a ubiquitous feature of the modern world, fueled by variables like environmental shifts, economic inequality, and violence. Unfortunately, this migratory flow is accompanied by a disturbing trend, the disappearance of migrants during their journey.
+        
+        One major knowledge gap about the difficulties experienced by disadvantaged people in transit is the lack of thorough study on missing migrants. Although media reporting and anecdotal evidence can provide insight into specific situations, a thorough investigation is crucial to fully grasp the complicate factors that contribute to this phenomenon.
+        """
     )
 
 
-if __name__ == "__main__":
-    run()
+
