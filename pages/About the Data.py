a82328548641e94bd21cd9117edb422fdecda2f8
NZ <147848898+yzassin@users.noreply.github.com>
2024-01-12 12:40:49 +0800

    Update and rename About the Dataset.py to About the Data.py

--- pages/About the Data.py
+++ pages/About the Data.py
@@ -2,18 +2,26 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-st.title('About Missing Migrant Dataset')
-st.subheader('The dataset includes some of the important informations such as:')
+st.set_page_config(
+        page_title="Data",
+        page_icon="📈",
+    )
+st.title('Missing Migrant Data')
+st.subheader('The data includes some of the important informations such as:')
 st.markdown('''
 **1. Demographic information about the missing migrants** – Gender, Country of origin.\n
-**2. Temporal information on when was the incident occured** – By year and month.o\n
+**2. Temporal information on when was the incident occured** – By year and month\n
 **3. Geospatial information on where was the incident occured** - The migration route taken, coordinates of the incident location.\n
+
+- The dataset can be obtain [here](https://www.kaggle.com/datasets/nelgiriyewithana/global-missing-migrants-dataset)
 ''')
+
+st.divider()
+
 def load_data():
     df = pd.read_csv('migrants.csv')
     return df
 df = load_data()
-st.success(f'The dataset consist of {df.shape[0]} rows by {df.shape[1]} columns')
 
 def plot_missing_migrant_features(features):
     feature = st.selectbox('**Choose Feature**', tuple(features))
@@ -38,7 +46,7 @@ def plot_missing_migrant_features(features):
 st.subheader('Missing Migrant Features')
 migrant_features = [
     'Region of Origin', 'Region of Incident',
-    'Cause of Death', 'Migration route', 'Coordinates', 'Gender'
+    'Cause of Death', 'Migration route', 'Gender'
 ]
 
 plot_missing_migrant_features(migrant_features)
