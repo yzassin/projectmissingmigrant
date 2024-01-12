741eb307d5fe2467a61a1ef714ccfeff2c20370b
NZ <147848898+yzassin@users.noreply.github.com>
2024-01-12 13:07:43 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -9,9 +9,9 @@ st.header("*Navigating Humanity*")
 st.image('pic.png', use_column_width=True)
 st.subheader("The Missing Migrant Project WebApp")
 st.markdown('''
-- This webapp serves as a comprehensive guide to explore and understand the incidents of missing migrants that have occurred across the globe for several years.\n
-- The beneficiaries of this webapp include researchers, policymakers, humanitarian organizations, and the general public\n
-- Domain scope: Social services, Migration and Government\n
+- This webapp serves as a comprehensive guide to explore and understand the incidents of missing migrants that have occurred across the globe for several years\n
+- The beneficiaries: Researchers, policymakers, humanitarian organizations, and the general public\n
+- Domain scope: Social services, migration and government\n
 ''')
 st.divider()
 st.markdown('WQD7003 DATA ANALYTICS Group 2 team:')
