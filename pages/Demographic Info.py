3878e1b6144d04738fcc8c935faae5532ab37439
NZ <147848898+yzassin@users.noreply.github.com>
2024-01-12 13:00:57 +0800

    Update Demographic Info.py

--- pages/Demographic Info.py
+++ pages/Demographic Info.py
@@ -1,56 +1,68 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import time
-
-import numpy as np
-
+import altair as alt
 import streamlit as st
-from streamlit.hello.utils import show_code
+import pandas as pd
 
+st.set_page_config(
+        page_title="Demographic",
+        page_icon="👨‍👩‍👦",
+    )
+df = pd.read_csv('migrants.csv')
 
-def plotting_demo():
-    progress_bar = st.sidebar.progress(0)
-    status_text = st.sidebar.empty()
-    last_rows = np.random.randn(1, 1)
-    chart = st.line_chart(last_rows)
+st.subheader("Demographic Analysis over the year")
+selected_columns = ['Number of Females', 'Number of Males', 'Number of Children']
+grouped_df = df.groupby('Incident year')[selected_columns].sum().reset_index()
 
-    for i in range(1, 101):
-        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-        status_text.text("%i%% Complete" % i)
-        chart.add_rows(new_rows)
-        progress_bar.progress(i)
-        last_rows = new_rows
-        time.sleep(0.05)
+# Melt the DataFrame to have a 'variable' column for demographics
+melted_df = pd.melt(grouped_df, id_vars='Incident year', value_vars=selected_columns, var_name='Demographic', value_name='Count')
 
-    progress_bar.empty()
-
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
-
-
-st.set_page_config(page_title="Plotting Demo", page_icon="📈")
-st.markdown("# Plotting Demo")
-st.sidebar.header("Plotting Demo")
-st.write(
-    """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
+# Plotting with Altair
+chart = (
+    alt.Chart(
+        melted_df,
+        title="",
+    )
+    .mark_line(point=True)
+    .encode(
+        x=alt.X("Incident year:O", title="Incident Year"),
+        y=alt.Y("Count", title="Count"),
+        color="Demographic",
+        tooltip=["Incident year", "Count", "Demographic"],
+    )
 )
+# Display the Altair chart using Streamlit
+st.altair_chart(chart, use_container_width=True)
 
-plotting_demo()
+#-------------------------------------------------
+st.divider()
+st.subheader("Popular Migration route by Migrant's Origin")
 
-show_code(plotting_demo)
+selected_columns = ['Number of Females', 'Number of Males', 'Number of Children']
+total_migrants = df[selected_columns].sum().sum()
+
+# Filter by migration route
+selected_route = st.selectbox("Select Migration Route", df['Migration route'].unique())
+filtered_data = df[df['Migration route'] == selected_route]
+
+# Filter out certain values in "Country of Origin"
+excluded_countries = ['Unknown']
+filtered_data = filtered_data[~filtered_data['Region of Origin'].isin(excluded_countries)]
+
+excluded_route = ['nan']
+filtered_data = filtered_data[~filtered_data['Migration route'].isin(excluded_route)]
+# Display a line chart based on the selected migration route
+chart = (
+    alt.Chart(
+        filtered_data,
+        title=f"Migration Route: {selected_route} Over Years",
+    )
+    .mark_line(point=True)
+    .encode(
+        x=alt.X("Incident year", title="Year"),  # 'O' means ordinal for discrete years
+        color=alt.Color("Region of Origin:N", title="Region of Origin"),
+        y=alt.Y(selected_columns[0], title="Number of Migrants"),  # Using the first selected column initially
+
+        tooltip=["Incident year", "Region of Origin"],
+    )
+)
+# Display the Altair chart using Streamlit
+st.altair_chart(chart, use_container_width=True)
